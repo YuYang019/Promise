29e71e3141ce6fc6a33e84d077544613554e3d23
maoyuyang <784406842@qq.com>
2018-03-27 19:31:15 +0800

    commit

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Learn Promise
+
+参考网上教程
+
+[Promise/A+标准](https://promisesaplus.com/#point-46)
+
+几点细节：
+1. then和catch总会返回一个全新的Promise对象, 这个新的Promise有自己的resolve, reject, callback队列等等
+2. then的回调是异步执行, 实际中是推入micro队列，但是这里就简单地用了setTimeout实现异步
+
+所以，为什么上一个resolve值出去，下一个then里就能接收到？
+
+不同版本实现可能不同，但是在这个版本里，上一个触发resolve，就把自己的onResolvedCallback清空，这个队列里包含的是一个或几个定义好的函数，这个函数主要内容，触发下一个then里的回调，获取回调的值，如果then回调有值，则继续触发then所处Promise的resolve，而这个resolve又会清空自己的onResolvedCallback，这样不断往下运行
+
+为什么then里throw的错误，即使隔了很多个then，能被catch收到？
+
+在某个then里或者说在某个Promise实例里，throw一个错误，会被try catch捕获，然后触发这个Promise实例的reject方法，这个reject会把自己的onRejectedCallback清空，这个队列里包含的是一个或几个定义好的函数，主要内容为，触发下一个then的onReject方法，这个onReject默认就是抛出一个错误，然后这又会被try catch捕获，触发下一个then的reject方法，而这个reject又会清空自己的onRejectedCallback，触发下下个then的onReject，这样不断反复的往下，直到传到catch
+
+当然，如果后面某个then里已经定义了onReject方法，那么就不会传到catch。这一点和原生的行为是一致的
+
+值的穿透？就是then的默认参数就是把值往后传或者抛
--- index.js
+++ index.js
@@ -1,53 +1,9 @@
 let uid = 0
 
-function Promise(executor) {
-  var self = this
-
-  this.uid = uid++
-
-  self.status = 'pending'
-  self.onResolvedCallback = []
-  self.onRejectedCallback = []
-
-  function resolve(value) {
-    if (value instanceof Promise) {
-      return value.then(resolve, reject)
-    }
-    setTimeout(function() { // 异步执行所有的回调函数
-      if (self.status === 'pending') {
-        self.status = 'resolved'
-        self.data = value
-        for (var i = 0; i < self.onResolvedCallback.length; i++) {
-          self.onResolvedCallback[i](value)
-        }
-      }
-    })
-  }
-
-  function reject(reason) {
-    setTimeout(function() { // 异步执行所有的回调函数
-      if (self.status === 'pending') {
-        self.status = 'rejected'
-        self.data = reason
-        for (var i = 0; i < self.onRejectedCallback.length; i++) {
-          self.onRejectedCallback[i](reason)
-        }
-      }
-    })
-  }
-
-  try {
-    executor(resolve, reject)
-  } catch (reason) {
-    reject(reason)
-  }
-}
-
+// 这个函数中间的部分不是很懂，作用应该是兼容不同第三方Promise的
 function resolvePromise(promise2, x, resolve, reject) {
-  var then
-  var thenCalledOrThrow = false
-
-  console.log(resolve, reject)
+  let then
+  let thenCalledOrThrow = false
 
   if (promise2 === x) {
     return reject(new TypeError('Chaining cycle detected for promise!'))
@@ -90,81 +46,258 @@ function resolvePromise(promise2, x, resolve, reject) {
   }
 }
 
-Promise.prototype.then = function(onResolved, onRejected) {
-  var self = this
-  var promise2
-  onResolved = typeof onResolved === 'function' ? onResolved : function(v) {
-    return v
-  }
-  onRejected = typeof onRejected === 'function' ? onRejected : function(r) {
-    throw r
-  }
+class Promise {
+  constructor(executor) {
+    this.uid = uid++
+    this.data = null
+    this.status = 'pending'
+    this.onResolvedCallback = []
+    this.onRejectedCallback = []
 
-  if (self.status === 'resolved') {
-    return promise2 = new Promise(function(resolve, reject) {
-      setTimeout(function() { // 异步执行onResolved
-        try {
-          var x = onResolved(self.data)
-          resolvePromise(promise2, x, resolve, reject)
-        } catch (reason) {
-          reject(reason)
+    const resolve = (value) => {
+      if (value instanceof Promise) {
+        return value.then(resolve, reject)
+      }
+
+      /**为什么要异步？举个例子
+       * 
+       * let isFulfilled = false;
+       * 
+       * let d = new Promise(function(resolve, reject) {
+       *   setTimeout(function() {
+       *     resolve(2);
+       *     isFulfilled = true;
+       *   }, 50);
+       * });
+       * 
+       * d.then(function onFulfilled() {
+       *   console.log(isFulfilled == true)
+       * });
+       * 
+       * 此时then里的判断为false，isFulfilled没有及时改变
+       */
+      setTimeout(() => {
+        if (this.status === 'pending') {
+          this.status = 'resolved'
+          this.data = value
+          this.onResolvedCallback.forEach(fn => {
+            fn(value)
+          })
         }
-      })
-    })
+      }, 0)
+    }
+
+    const reject = (reason) => {
+      setTimeout(() => {
+        if (this.status === 'pending') {
+          this.status = 'rejected'
+          this.data = reason
+          this.onRejectedCallback.forEach(fn => {
+            fn(reason)
+          })
+        }
+      }, 0)
+    }
+
+    try {
+      executor(resolve, reject)
+    } catch (err) {
+      reject(err)
+    }
   }
 
-  if (self.status === 'rejected') {
-    return promise2 = new Promise(function(resolve, reject) {
-      setTimeout(function() { // 异步执行onRejected
-        try {
-          var x = onRejected(self.data)
-          resolvePromise(promise2, x, resolve, reject)
-        } catch (reason) {
-          reject(reason)
-        }
-      })
-    })
-  }
+  then(onResolved, onRejected) {
+    let promise2
 
-  if (self.status === 'pending') {
-    // 这里之所以没有异步执行，是因为这些函数必然会被resolve或reject调用，而resolve或reject函数里的内容已是异步执行，构造函数里的定义
-    return promise2 = new Promise(function(resolve, reject) {
-      self.onResolvedCallback.push(function(value) {
-        try {
-          var x = onResolved(value)
-          resolvePromise(promise2, x, resolve, reject)
-        } catch (r) {
-          reject(r)
-        }
-      })
+    onResolved = typeof onResolved === 'function' ? onResolved : function(v) { return v }
+    onRejected = typeof onRejected === 'function' ? onRejected : function(r) { throw r }
 
-      self.onRejectedCallback.push(function(reason) {
+    if (this.status === 'resolved') {
+      return promise2 = new Promise((resolve, reject) => {
+        setTimeout(() => {
           try {
-            var x = onRejected(reason)
+            let x = onResolved(this.data)
             resolvePromise(promise2, x, resolve, reject)
-          } catch (r) {
-            reject(r)
+          } catch (err) {
+            reject(err)
           }
+        }, 0)
+      })
+    }
+
+    if (this.status === 'rejected') {
+      return promise2 = new Promise((resolve, reject) => {
+        setTimeout(() => {
+          try {
+            let x = onRejected(this.data)
+            resolvePromise(promise2, x, resolve, reject)
+          } catch (err) {
+            reject(err)
+          }
+        }, 0)
+      })
+    }
+
+    if (this.status === 'pending') {
+      return promise2 = new Promise((resolve, reject) => {
+
+        this.onResolvedCallback.push((value) => {
+          setTimeout(() => {
+            try {
+              let x = onResolved(value)
+              resolvePromise(promise2, x, resolve, reject)
+            } catch (err) {
+              reject(err)
+            }
+          }, 0)
         })
-    })
+
+        this.onRejectedCallback.push((reason) => {
+          setTimeout(() => {
+            try {
+              let x = onRejected(reason)
+              resolvePromise(promise2, x, resolve, reject)
+            } catch (err) {
+              reject(err)
+            }
+          }, 0)
+        })
+      })
+    }
+  }
+
+  catch(onRejected) {
+    return this.then(null, onRejected)
+  }
+
+  finally(callback) {
+    let P = Promise
+    return this.then(
+      value => P.resolve(callback()),
+      reason => P.resolve(callback())
+    )
   }
 }
 
-Promise.prototype.catch = function(onRejected) {
-  return this.then(null, onRejected)
+Promise.resolve = function(value) {
+  return new Promise((resolve, reject) => { resolve(value) })
 }
 
-const promiseA = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve(1)
-  }, 400)
-}).then(val => {
+Promise.reject = function(value) {
+  return new Promise((resolve, reject) => { reject(value) })
+}
+
+Promise.all = function(array /* [p1, p2, ...] */) {
+  return new Promise((resolve, reject) => {
+    if (!Array.isArray(array)) {
+      return reject(new TypeError('arguments must be a array'))
+    }
+    let count = 0
+    const length = array.length
+    const resolvedVal = []
+    for (let i = 0; i < length; i++) {
+      Promise.resolve(array[i]).then((val) => {
+        count++
+        resolvedVal[i] = val
+        if (count === length) {
+          return resolve(resolvedVal)
+        }
+      }, (reason) => {
+        return reject(reason)
+      })
+    }
+  })
+}
+
+Promise.race = function(array /* [p1, p2, ...] */) {
+  return new Promise((resolve, reject) => {
+    if (!Array.isArray(array)) {
+      return reject(new TypeError('arguments must be a array'))
+    }
+    const length = array.length
+    for (let i = 0; i < length; i++) {
+      Promise.resolve(array[i]).then(val => {
+        resolve(val)
+      }, (reason) => {
+        reject(reason)
+      })
+    }
+  })
+}
+
+// test Promise.finally
+const p = new Promise((rs, rj) => {
+  rs(1)
+})
+.then(val => {
+  throw val
+  return 3
+})
+.then(val => {
   console.log(val)
-  return 2
+  return val
+})
+.catch(e => {
+  console.log(e)
 })
 .then(val => {
   console.log(val)
 })
-.catch(e => {
-  console.log(e)
-}) 
+.finally(() => {
+  console.log('aaaa')
+})
+
+// test Promise.all and Promise.race
+const p1 = new Promise((rs, rj) => {
+  setTimeout(() => {
+    rs(1)
+  }, 1000)
+})
+
+const p2 = new Promise((rs, rj) => {
+  setTimeout(() => {
+    rs(2)
+  }, 2000)
+})
+
+const p3 = new Promise((rs, rj) => {
+  setTimeout(() => {
+    rs(3)
+  }, 2000)
+})
+
+const p4 = new Promise((rs, rj) => {
+  setTimeout(() => {
+    rj(4)
+  }, 500)
+})
+
+
+Promise.all([p1, p2, p3]).then(val => {
+  console.log(val)
+}).catch(err => {
+  console.log(err)
+})
+
+Promise.race([p1, p2, p3, p4]).then(val => {
+  console.log(val)
+}).catch(err => {
+  console.log(err)
+})
+
+
+// use promises-aplus-tests to test Promise
+Promise.deferred =  Promise.defer = function() {
+  const dfd = {}
+  dfd.promise = new Promise((resolve, reject) => {
+    dfd.resolve = resolve
+    dfd.reject = reject
+  })
+  return dfd
+}
+
+try {
+  module.exports = Promise
+} catch (e) {}
+
+
